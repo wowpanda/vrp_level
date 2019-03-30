a33e13089401b3173084a77b28353965756f9f5e
brefaby <40739200+brefaby@users.noreply.github.com>
2019-03-30 10:58:51 +0200

    Update client.lua

--- vrp_level/client.lua
+++ vrp_level/client.lua
@@ -7,33 +7,7 @@ end)
 
 RegisterNetEvent('notify:level_up')
 AddEventHandler('notify:level_up', function(level)
-    Citizen.CreateThread(function()
-        Citizen.Wait(0)
-            function Initialize(scaleform)
-                local scaleform = RequestScaleformMovie(scaleform)
-                while not HasScaleformMovieLoaded(scaleform) do
-                    Citizen.Wait(0)
-                end
-                PushScaleformMovieFunction(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
-                PushScaleformMovieFunctionParameterString("~y~Zentrix ~w~| ~b~Level up")
-                PushScaleformMovieFunctionParameterString("~w~Ai luat ~b~level "..level.."~w~!")
-                PopScaleformMovieFunctionVoid()
-                PlaySoundFrontend(-1, "UNDER_THE_BRIDGE", "HUD_AWARDS", 1)
-                Citizen.SetTimeout(5000, function()
-                    PushScaleformMovieFunction(scaleform, "SHARD_ANIM_OUT")
-                    PushScaleformMovieFunctionParameterInt(1)
-                    PushScaleformMovieFunctionParameterFloat(0.33)
-                    PopScaleformMovieFunctionVoid()
-                    PlaySoundFrontend(-1, "1st_Person_Transition", "PLAYER_SWITCH_CUSTOM_SOUNDSET", 1)
-                end)
-                return scaleform
-            end
-
-            scaleform = Initialize("mp_big_message_freemode")
-
-            while true do
-                Citizen.Wait(0)
-                DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255, 0)
-            end
-    end)
+  SetNotificationTextEntry("STRING")
+  AddTextComponentString(level)
+  DrawNotification(true, false)
 end)
